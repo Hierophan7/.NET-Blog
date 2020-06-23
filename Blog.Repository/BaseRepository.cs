dffb282d189c7edd20a1697f6d935f51f0b5fb5b
Vadym Bei <bey1705@gmail.com>
2020-06-23 16:49:13 +0300

    First commit

---
+++ Blog.Repository/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Repository
+{
+	public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+	{
+
+
+		public void Create(TEntity entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Delete(TEntity entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IQueryable<TEntity> FindAll(params Expression<Func<TEntity, object>>[] includes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<TEntity>> FindAllAsync(params Expression<Func<TEntity, object>>[] includes)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> expression)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task SaveAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(TEntity entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Blog.Repository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Blog.Repository
+{
+	public interface IBaseRepository<TEntity>
+	{
+		Task<IEnumerable<TEntity>> FindAllAsync(params Expression<Func<TEntity, object>>[] includes);
+		IQueryable<TEntity> FindAll(params Expression<Func<TEntity, object>>[] includes);
+		Task<IEnumerable<TEntity>> FindByConditionAsync(Expression<Func<TEntity, bool>> expression);
+		void Create(TEntity entity);
+		void Update(TEntity entity);
+		bool Delete(TEntity entity);
+		Task SaveAsync();
+	}
+}
